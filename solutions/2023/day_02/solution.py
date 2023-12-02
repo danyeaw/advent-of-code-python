ef85f649f61ae9ba4372e83032eafaacf38f2d75
Dan Yeaw <dan@yeaw.me>
2023-12-02 12:23:19 -0500

    Refactor to using a generator

--- solutions/2023/day_02/solution.py
+++ solutions/2023/day_02/solution.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum
+from typing import Generator
 
 from ...base import StrSplitSolution, answer
 
@@ -24,29 +25,28 @@ def parse(line: str) -> tuple[int, list[str]]:
 
 
 def game_possible(game_record: list[str]) -> bool:
-    for subset in game_record:
-        subset_colors: list[str] = subset.split(", ")
-        for color in Colors:
-            for num_by_color in subset_colors:
-                if color.name in num_by_color:
-                    block_count = int(num_by_color.split(" ")[0])
-                    if block_count > MaxColors[color.name].value:
-                        return False
-    return True
+    return all(
+        block_count <= MaxColors[color.name].value
+        for block_count, color in get_block_count(game_record)
+    )
 
 
 def get_min_blocks_by_color(game_record: list[str]) -> list[int]:
     min_blocks = [0, 0, 0]
+    for block_count, color in get_block_count(game_record):
+        color_index = Colors[color.name].value
+        min_blocks[color_index] = max(block_count, min_blocks[color_index])
+    return min_blocks
+
+
+def get_block_count(game_record: list[str]) -> Generator[tuple[int, Enum], None, None]:
     for subset in game_record:
         subset_colors: list[str] = subset.split(", ")
         for color in Colors:
             for num_by_color in subset_colors:
                 if color.name in num_by_color:
-                    block_count = int(num_by_color.split(" ")[0])
-                    color_index = Colors[color.name].value
-                    min_blocks[color_index] = max(block_count, min_blocks[color_index])
-    return min_blocks
-
+                    yield int(num_by_color.split(" ")[0]), color
+    
 
 def power_of_numbers(numbers: list[int]) -> int:
     result = 1
