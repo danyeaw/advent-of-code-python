f36e11eacedf5cc5e47d335251717ac0d7810d22
Dan Yeaw <dan@yeaw.me>
2023-12-02 13:12:07 -0500

    Simplify solution

--- solutions/2023/day_02/solution.py
+++ solutions/2023/day_02/solution.py
@@ -1,58 +1,20 @@
 from __future__ import annotations
 
-from enum import Enum
-from typing import Generator
+import re
+from collections import defaultdict
 
 from ...base import StrSplitSolution, answer
 
-
-class Colors(Enum):
-    red = 0
-    green = 1
-    blue = 2
-
-
-class MaxColors(Enum):
-    red = 12
-    green = 13
-    blue = 14
-
-
-def parse(line: str) -> tuple[int, list[str]]:
-    game_num: int = int(line.split(":")[0].split(" ")[1])
-    game_record: list[str] = line.split(": ")[1].split("; ")
-    return game_num, game_record
-
-
-def game_possible(game_record: list[str]) -> bool:
-    return all(
-        block_count <= MaxColors[color.name].value
-        for block_count, color in get_block_count(game_record)
-    )
-
-
-def get_min_blocks_by_color(game_record: list[str]) -> list[int]:
-    min_blocks = [0, 0, 0]
-    for block_count, color in get_block_count(game_record):
-        color_index = Colors[color.name].value
-        min_blocks[color_index] = max(block_count, min_blocks[color_index])
-    return min_blocks
-
-
-def get_block_count(game_record: list[str]) -> Generator[tuple[int, Enum], None, None]:
-    for subset in game_record:
-        subset_colors: list[str] = subset.split(", ")
-        for color in Colors:
-            for num_by_color in subset_colors:
-                if color.name in num_by_color:
-                    yield int(num_by_color.split(" ")[0]), color
-    
 
 def power_of_numbers(numbers: list[int]) -> int:
     result = 1
     for number in numbers:
         result *= number
     return result
+
+
+def get_count_color(game_record: str) -> list[tuple[str, str]]:
+    return re.findall(r"(\d+) (\w+)", game_record)
 
 
 class Solution(StrSplitSolution):
@@ -62,17 +24,24 @@ class Solution(StrSplitSolution):
     @answer(2528)
     def part_1(self) -> int:
         valid_sum = 0
-        for line in self.input:
-            game_num, game_record = parse(line)
-            if game_possible(game_record):
-                valid_sum += game_num
+        for game_num, line in enumerate(self.input, start=1):
+            _, game_record = line.split(": ")
+            if all(
+                    int(count) <= {"red": 12, "green": 13, "blue": 14}[color]
+                    for count, color in get_count_color(game_record)
+            ):
+                valid_sum += int(game_num)
         return valid_sum
 
     @answer(67363)
     def part_2(self) -> int:
         result = 0
-        for line in self.input:
-            game_num, game_record = parse(line)
-            min_blocks_by_color = get_min_blocks_by_color(game_record)
-            result += power_of_numbers(min_blocks_by_color)
+        for game_num, line in enumerate(self.input, start=1):
+            _, game_record = line.split(": ")
+            min_per_color: dict[str, int] = defaultdict(int)
+
+            for count, color in get_count_color(game_record):
+                min_per_color[color] = max(min_per_color[color], int(count))
+            result += power_of_numbers(list(min_per_color.values()))
+
         return result
