29179c2378d74fdbb846a38bcd8d529eaa5c4c45
Dan Yeaw <dan@yeaw.me>
2023-12-01 21:35:42 -0500

    Add part 2 for 2023 day 1

--- solutions/2023/day_01/README.md
+++ solutions/2023/day_01/README.md
@@ -12,3 +12,13 @@ of variables to hold the first and last values, and conditional statements.
 
 The optimized solution uses a single list comprehension to put all the digits
 in a list, and then use indexing to get the first and last digits.
+
+## Part 2
+
+Part two was a bit tricky, because in some of the lines the written out words overlap.
+For example, eightwothree. If you search and replace it from low to high, you'll replace
+the two and three with digits, and get eigh23. However, eight is the first digit, so the
+answer should be 8wo3 or 83.
+
+One way to resolve this is when replacing the word with the digit, keep the first and
+last letters of the word, since those are the ones that could overlap.
--- solutions/2023/day_01/solution.py
+++ solutions/2023/day_01/solution.py
@@ -1,9 +1,29 @@
 from ...base import StrSplitSolution, answer
 
+str_to_digit = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
 
 def find_calibration_value(line: str) -> int:
     digits = [digit for digit in line if digit.isdigit()]
-    return int(f"{digits[0]}{digits[-1]}")
+    return int(f"{digits[0]}{digits[-1]}") if digits else 0
+
+
+def replace_words(line: str) -> str:
+    for word, digit in str_to_digit.items():
+        if word in line:
+            # Don't remove the first and last letters since words can overlap
+            line = line.replace(word, f"{word[0]}{digit}{word[-1]}")
+    return line
 
 
 class Solution(StrSplitSolution):
@@ -13,3 +33,7 @@ class Solution(StrSplitSolution):
     @answer(55447)
     def part_1(self) -> int:
         return sum(find_calibration_value(line) for line in self.input)
+
+    @answer(54706)
+    def part_2(self) -> int:
+        return sum(find_calibration_value(replace_words(line)) for line in self.input)
