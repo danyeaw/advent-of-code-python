8f6b6c8f6e3b4417d8adc8f632b90ce21bdbfc74
Dan Yeaw <dan@yeaw.me>
2023-12-03 13:56:03 -0500

    Simplify solution using dicts and overlay

--- solutions/2023/day_03/solution.py
+++ solutions/2023/day_03/solution.py
@@ -1,9 +1,29 @@
 # puzzle prompt: https://adventofcode.com/2023/day/3
 import re
+from math import prod
 
 from ...base import StrSplitSolution, answer
 
-SYMBOLS = ("*", "@", "#", "$", "%", "&", "*", "/", "=", "+", "-")
+
+def get_coord_adjacent_symbol_to_number(board: str) -> dict[str, list]:
+    width = len(board[0])
+    height = len(board)
+    coord_adjacent_symbol_to_number = {
+        (r, c): []
+        for r in range(width)
+        for c in range(height)
+        if board[r][c] not in "01234566789."
+    }
+    for row_num, row in enumerate(board):
+        for num in re.finditer(r"\d+", row):
+            adjacent_area = {
+                (r, c)
+                for r in (row_num - 1, row_num, row_num + 1)
+                for c in range(num.start() - 1, num.end() + 1)
+            }
+            for coord in adjacent_area & coord_adjacent_symbol_to_number.keys():
+                coord_adjacent_symbol_to_number[coord].append(int(num.group()))
+    return coord_adjacent_symbol_to_number
 
 
 class Solution(StrSplitSolution):
@@ -12,76 +32,16 @@ class Solution(StrSplitSolution):
 
     @answer(520019)
     def part_1(self) -> int:
-        matching_numbers = []
-        previous_row = None
-        current_row = None
-        width = len(self.input[0])
-        self.input.append("." * width)
-        for next_row in self.input:
-            if current_row:
-                for match in re.finditer(r"\d+", current_row):
-                    area = []
-                    start_idx = int(match.start())
-                    end_idx = int(match.end())
-                    left_area_idx = max(start_idx - 1, 0)
-                    right_area_idx = min(end_idx + 1, len(current_row))
-                    if previous_row:
-                        area.append(previous_row[left_area_idx:right_area_idx])
-                    area.extend(
-                        (
-                            current_row[left_area_idx:right_area_idx],
-                            next_row[left_area_idx:right_area_idx],
-                        )
-                    )
-                    matching_numbers.extend(
-                        int(current_row[start_idx:end_idx])
-                        for row in area
-                        for char in row
-                        if char in SYMBOLS
-                    )
-            previous_row = current_row
-            current_row = next_row
-        return sum(matching_numbers)
+        return sum(
+            sum(value)
+            for value in get_coord_adjacent_symbol_to_number(self.input).values()
+        )
 
     @answer(75519888)
     def part_2(self) -> int:
-        matching_numbers = []
-        previous_row = None
-        current_row = None
-        width = len(self.input[0])
-        self.input.append("." * width)
-        for next_row in self.input:
-            if current_row:
-                for match in re.finditer(r"\*", current_row):
-                    full_rows = []
-                    area = []
-                    start_idx = int(match.start())
-                    end_idx = int(match.end())
-                    left_area_idx = max(start_idx - 1, 0)
-                    right_area_idx = min(end_idx + 1, len(current_row))
-                    if previous_row:
-                        full_rows.append(previous_row)
-                        area.append(previous_row[left_area_idx:right_area_idx])
-                    area.extend(
-                        (
-                            current_row[left_area_idx:right_area_idx],
-                            next_row[left_area_idx:right_area_idx],
-                        )
-                    )
-                    full_rows.extend([current_row, next_row])
-                    numbers = []
-                    for row in full_rows:
-                        for int_match in re.finditer(r"\d+", row):
-                            num_start = int_match.start()
-                            num_end = int_match.end()
-                            if (
-                                left_area_idx <= num_start < right_area_idx
-                                or left_area_idx < num_end <= right_area_idx
-                            ):
-                                numbers.append(int(int_match.group()))
-                    if len(numbers) == 2:
-                        matching_numbers.append(numbers[0] * numbers[1])
-
-            previous_row = current_row
-            current_row = next_row
-        return sum(matching_numbers)
+        # There are no symbols except *'s that are adjacent to exactly two numbers
+        return sum(
+            prod(value)
+            for value in get_coord_adjacent_symbol_to_number(self.input).values()
+            if len(value) == 2
+        )
