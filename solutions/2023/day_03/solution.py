a3a8508674c37bfd5ebaf79d635f8dffb1d29e6a
Dan Yeaw <dan@yeaw.me>
2023-12-03 10:03:21 -0500

    Add 2023 day 3 part 1 initial solution

---
+++ solutions/2023/day_03/solution.py
@@ -0,0 +1,57 @@
+# puzzle prompt: https://adventofcode.com/2023/day/3
+import re
+
+from ...base import StrSplitSolution, answer
+
+SYMBOLS = ("*", "@", "#", "$", "%", "&", "*", "/", "=", "+", "-")
+
+
+class Solution(StrSplitSolution):
+    _year = 2023
+    _day = 3
+
+    @answer(520019)
+    def part_1(self) -> int:
+        matching_numbers = []
+        previous_row = None
+        current_row = None
+        width = len(self.input[0])
+        self.input.append("." * width)
+        for next_row in self.input:
+            if current_row:
+                for match in re.finditer(r"\d+", current_row):
+                    area = []
+                    left_num_idx = int(match.span()[0])
+                    right_num_idx = int(match.span()[1])
+                    left_area_idx = (
+                        left_num_idx - 1 if left_num_idx > 0 else left_num_idx
+                    )
+                    if right_num_idx < len(current_row) - 1:
+                        right_area_idx = right_num_idx + 1
+                    else:
+                        right_area_idx = right_num_idx
+                    if previous_row:
+                        area.append(previous_row[left_area_idx:right_area_idx])
+                    area.extend(
+                        (
+                            current_row[left_area_idx:right_area_idx],
+                            next_row[left_area_idx:right_area_idx],
+                        )
+                    )
+                    matching_numbers.extend(
+                        int(current_row[left_num_idx:right_num_idx])
+                        for row in area
+                        for char in row
+                        if char in SYMBOLS
+                    )
+            previous_row = current_row
+            current_row = next_row
+        return sum(matching_numbers)
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
