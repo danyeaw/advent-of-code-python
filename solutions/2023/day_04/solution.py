a6af39c23fe1c7ce7e3c04ff7b781167e0cd7aa7
Dan Yeaw <dan@yeaw.me>
2023-12-04 20:57:36 -0500

    Add initial solution for day 4 part 2

--- solutions/2023/day_04/solution.py
+++ solutions/2023/day_04/solution.py
@@ -1,10 +1,15 @@
-# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
-import re
-from collections import Counter
-
 # puzzle prompt: https://adventofcode.com/2023/day/4
+from ...base import StrSplitSolution, answer
 
-from ...base import answer, StrSplitSolution
+
+def parse(line: str) -> tuple[list[int], list[int]]:
+    _, numbers = line.replace("  ", " ").split(": ")
+    winners, my_numbers = numbers.split(" | ")
+    winners = winners.split(" ")
+    my_numbers = my_numbers.split(" ")
+    winners = [int(num) for num in winners]
+    my_numbers = [int(num) for num in my_numbers]
+    return winners, my_numbers
 
 
 class Solution(StrSplitSolution):
@@ -15,20 +20,20 @@ class Solution(StrSplitSolution):
     def part_1(self) -> int:
         total_worth = []
         for line in self.input:
-            _, numbers = line.replace("  ", " ").split(": ")
-            winners, my_numbers = numbers.split(" | ")
-            winners = winners.split(" ")
-            my_numbers = my_numbers.split(" ")
-            winners = [int(num) for num in winners]
-            my_numbers = [int(num) for num in my_numbers]
+            winners, my_numbers = parse(line)
             if intersect := set(winners).intersection(my_numbers):
-                total_worth.append(2**(len(intersect) - 1))
+                total_worth.append(2 ** (len(intersect) - 1))
         return sum(total_worth)
 
-    # @answer(1234)
+    @answer(10212704)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        scorecards = []
+        scorecards.extend(len(self.input) * [1])
+        for idx, line in enumerate(self.input):
+            winners, my_numbers = parse(line)
+            if intersect := set(winners).intersection(my_numbers):
+                stop = min(idx + len(intersect), len(self.input)) + 1
+                for _ in range(scorecards[idx]):
+                    for next_cards in range(idx + 1, stop):
+                        scorecards[next_cards] += 1
+        return sum(scorecards)
