c35b55c5d6177fafc1ece8ef610bdd9f12b85b55
Dan Yeaw <dan@yeaw.me>
2023-12-06 14:33:58 -0500

    Simplify and refactor part 1 solution

--- solutions/2023/day_05/solution.py
+++ solutions/2023/day_05/solution.py
@@ -1,22 +1,24 @@
 # puzzle prompt: https://adventofcode.com/2023/day/5
-import re
-from itertools import batched
-
 from ...base import TextSolution, answer
 
 
-def parse(text: str) -> list[list[list[int]]]:
-    seeds = [
-        int(seed)
-        for seed in re.search(r"seeds:.*\n", self.input)
-        .group()
-        .split(": ")[1]
-        .split()
-    ]
-    text = re.sub(r"seeds:.*\n\n", "", self.input)
-    text = re.sub(r".*:.*\n", "", text).split("\n\n")
-    maps = [[map.split() for map in group.split("\n")] for group in text]
-    return [[[int(val) for val in row] for row in map] for map in maps]
+def parse_range(line: str) -> tuple[range, int]:
+    dest_start, source_start, rng = [int(val) for val in line.split()]
+    return range(source_start, source_start + rng), dest_start - source_start
+
+
+def parse_map(map_block: str) -> list[tuple[range, int]]:
+    return sorted(
+        [parse_range(line) for line in map_block.split("\n")[1:]],
+        key=lambda r: r[0].start,
+    )
+
+
+def layer_transform(num: int, layer: list[tuple[range, int]]) -> int:
+    for mask, offset in layer:
+        if num in mask:
+            return num + offset
+    return num
 
 
 class Solution(TextSolution):
@@ -25,51 +27,17 @@ class Solution(TextSolution):
 
     @answer(251346198)
     def part_1(self) -> int:
-        seeds = [
-            int(seed)
-            for seed in re.search(r"seeds:.*\n", self.input)
-            .group()
-            .split(": ")[1]
-            .split()
-        ]
-        text = re.sub(r"seeds:.*\n\n", "", self.input)
-        text = re.sub(r".*:.*\n", "", text).split("\n\n")
-        maps = [[map.split() for map in group.split("\n")] for group in text]
-        maps = [[[int(val) for val in row] for row in map] for map in maps]
+        blocks = self.input.split("\n\n")
+        seeds = [int(seed) for seed in blocks[0][6:].split()]
+        map_layers = [parse_map(block) for block in blocks[1:]]
 
         locations = []
         for seed in seeds:
-            value = seed
-            for map in maps:
-                for row in map:
-                    if row[1] <= value <= row[1] + row[2]:
-                        value = row[0] + value - row[1]
-                        break
-            locations.append(value)
+            for layer in map_layers:
+                seed = layer_transform(seed, layer)
+            locations.append(seed)
         return min(locations)
 
     # @answer(1234)
     def part_2(self) -> int:
-        seeds = [
-            int(seed)
-            for seed in re.search(r"seeds:.*\n", self.input)
-            .group()
-            .split(": ")[1]
-            .split()
-        ]
-        text = re.sub(r"seeds:.*\n\n", "", self.input)
-        text = re.sub(r".*:.*\n", "", text).split("\n\n")
-        maps = [[map.split() for map in group.split("\n")] for group in text]
-        maps = [[[int(val) for val in row] for row in map] for map in maps]
-
-        locations = []
-        for start_seed, rng in batched(seeds, 2):
-            for seed in range(start_seed, start_seed + rng):
-                value = seed
-                for map in maps:
-                    for row in map:
-                        if row[1] <= value <= row[1] + row[2]:
-                            value = row[0] + value - row[1]
-                            break
-                locations.append(value)
-        return min(locations)
+        pass
