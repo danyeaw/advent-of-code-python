6afce7949a011230c064e91af7742b1c10923ecb
Dan Yeaw <dan@yeaw.me>
2023-12-06 20:29:55 -0500

    Refactor and simplify solution

--- solutions/2023/day_06/solution.py
+++ solutions/2023/day_06/solution.py
@@ -1,38 +1,26 @@
 # puzzle prompt: https://adventofcode.com/2023/day/6
 import math
-import re
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
 
 
-class Solution(TextSolution):
+def get_num_winners(time: int, distance: int) -> int:
+    return len([1 for button in range(time) if button * (time - button) > distance])
+
+
+class Solution(StrSplitSolution):
     _year = 2023
     _day = 6
 
     @answer(219849)
     def part_1(self) -> int:
-        lines = self.input.split("\n")
-        times = re.sub(r"\W+", " ", lines[0][4:])[1:].split()
-        times = [int(time) for time in times]
-        distances = re.sub(r"\W+", " ", lines[1][8:])[1:].split()
-        distances = [int(distance) for distance in distances]
-
-        winners = []
-        for race, time in enumerate(times):
-            race_winners = [
-                button
-                for button in range(time)
-                if button * (time - button) > distances[race]
-            ]
-            winners.append(len(race_winners))
+        races = zip(
+            *[[int(value) for value in line.split()[1:]] for line in self.input]
+        )
+        winners = [get_num_winners(time, distance) for time, distance in races]
         return math.prod(winners)
 
-    @answer(71503)
+    @answer(29432455)
     def part_2(self) -> int:
-        lines = self.input.split("\n")
-        time = int(re.sub(r"\W+", "", lines[0][4:]))
-        distance = int(re.sub(r"\W+", "", lines[1][8:]))
-        winners = [
-            button for button in range(time) if button * (time - button) > distance
-        ]
-        return len(winners)
+        time, distance = [int("".join(line.split()[1:])) for line in self.input]
+        return get_num_winners(time, distance)
