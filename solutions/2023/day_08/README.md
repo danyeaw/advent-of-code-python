7607bc1f1a51a278f8fd3479e830c3c18ecddf09
Dan Yeaw <dan@yeaw.me>
2023-12-09 13:30:57 -0500

    Add day 8 part 2

--- solutions/2023/day_08/README.md
+++ solutions/2023/day_08/README.md
@@ -10,3 +10,9 @@ dictionary comprehension.
 
 ## Part 2
 
+The change in this one to do 6 calculations at once was pretty straight forward. Unfortunately, the answer is in the
+trillions, and would have taken a very long time to brute force.
+
+Each of the 6 paths gets stuck in a very large loop that repeat themselves after 10,000+ counts through the possible
+combinations. Once you find the loop length for each input, you can take the Least Common Multiple (LCM) of the
+6 loop lengths to find the solution.
--- solutions/2023/day_08/solution.py
+++ solutions/2023/day_08/solution.py
@@ -1,7 +1,22 @@
 # puzzle prompt: https://adventofcode.com/2023/day/8
 from itertools import cycle
+from math import lcm
 
 from ...base import TextSolution, answer
+
+
+def parse(lines: str) -> tuple[list[int], dict[str, str]]:
+    inputs = lines.split("\n\n")
+    left_right = [
+        int(direction) for direction in inputs[0].replace("L", "0").replace("R", "1")
+    ]
+    nodes = inputs[1].split("\n")
+    nodes = [
+        [node.split(" = ")[0], tuple(node.split(" = ")[1].strip("()").split(", "))]
+        for node in nodes
+    ]
+    node_to_node = dict(nodes)
+    return left_right, node_to_node
 
 
 class Solution(TextSolution):
@@ -10,17 +25,7 @@ class Solution(TextSolution):
 
     @answer(19241)
     def part_1(self) -> int:
-        inputs = self.input.split("\n\n")
-        left_right = [
-            int(direction)
-            for direction in inputs[0].replace("L", "0").replace("R", "1")
-        ]
-        nodes = inputs[1].split("\n")
-        nodes = [
-            [node.split(" = ")[0], tuple(node.split(" = ")[1].strip("()").split(", "))]
-            for node in nodes
-        ]
-        node_to_node = dict(nodes)
+        left_right, node_to_node = parse(self.input)
         location = "AAA"
         count = 0
         for direction in cycle(left_right):
@@ -30,10 +35,33 @@ class Solution(TextSolution):
                 break
         return count
 
-    # @answer(1234)
+    @answer(9606140307013)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        left_right, node_to_node = parse(self.input)
+        locations = [
+            init_location
+            for init_location in node_to_node.keys()
+            if init_location[2] == "A"
+        ]
+        count = 0
+        start_end_loop = [[0, 0] for _ in range(6)]
+        for direction in cycle(left_right):
+            for idx, node in enumerate(locations):
+                locations[idx] = node_to_node[node][direction]
+            count += 1
+            for idx, location in enumerate(locations):
+                if location[2] == "Z":
+                    if start_end_loop[idx][0] == 0:
+                        start_end_loop[idx][0] = count
+                    elif start_end_loop[idx][1] == 0:
+                        start_end_loop[idx][1] = count
+            if all(
+                loop_length[0] != 0 and loop_length[1] != 0
+                for loop_length in start_end_loop
+            ):
+                break
+        loop_lengths = [
+            start_end_loop[idx][1] - start_end_loop[idx][0]
+            for idx in range(len(start_end_loop))
+        ]
+        return lcm(*loop_lengths)
