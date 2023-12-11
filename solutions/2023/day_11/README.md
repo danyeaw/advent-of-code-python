ae8b6fd8914c8b7a46a34b94536f57034f2a8735
Dan Yeaw <dan@yeaw.me>
2023-12-11 17:34:09 -0500

    Add scaffolding for day 11

---
+++ solutions/2023/day_11/README.md
@@ -0,0 +1,8 @@
+# Day 11 (2023)
+
+`Cosmic Expansion` ([prompt](https://adventofcode.com/2023/day/11))
+
+## Part 1
+
+## Part 2
+
---
+++ solutions/2023/day_11/solution.py
@@ -0,0 +1,17 @@
+# puzzle prompt: https://adventofcode.com/2023/day/11
+
+from ...base import StrSplitSolution, answer
+
+
+class Solution(StrSplitSolution):
+    _year = 2023
+    _day = 11
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
