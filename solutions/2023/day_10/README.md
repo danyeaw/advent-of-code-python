a614f016c73aa4e434baf7b1844b5d295e2edfaa
Dan Yeaw <dan@yeaw.me>
2023-12-10 21:43:15 -0500

    Add 2023 day 10 part 2

--- solutions/2023/day_10/README.md
+++ solutions/2023/day_10/README.md
@@ -12,3 +12,11 @@ path, and the farthest breadth is the answer.
 
 ## Part 2
 
+I took advantage of how vector graphics are drawn. You can iterate through the
+line and find polarity based on vertical pipes you intersect, odd is inside the
+loop. I iterated over each row kept a `is_inside` variable that starts false.
+We then iterate for each tile, and for each pipe that has a side pointing north
+and is in the list of pipes in the loop from part 1, we invert `is_inside`.
+
+For any positions that are inside and not pipes, we add these positions to the
+enclosed list and then find the length of that to get the solution.
--- solutions/2023/day_10/solution.py
+++ solutions/2023/day_10/solution.py
@@ -1,10 +1,66 @@
-# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
-
 # puzzle prompt: https://adventofcode.com/2023/day/10
 
 from collections import deque
 
 from ...base import StrSplitSolution, answer
+
+
+def find_loop(input: list[str]):
+    valid_up = "|7FS"
+    valid_down = "|LJS"
+    valid_left = "-LFS"
+    valid_right = "-J7S"
+    maze_size = [0, 0]
+    maze = {}
+    for y, line in enumerate(input):
+        for x, char in enumerate(line):
+            if char != ".":
+                maze[(x, y)] = char, []
+            if x > maze_size[0] or y > maze_size[1]:
+                maze_size = [x, y]
+    start = ()
+    for space, (char, connected_neighbors) in maze.items():
+        x, y = space
+        if (
+            char in valid_down
+            and maze.get((x, y - 1))
+            and maze[x, y - 1][0] in valid_up
+        ):
+            maze[x, y][1].append((x, y - 1))
+        if (
+            char in valid_up
+            and maze.get((x, y + 1))
+            and maze[x, y + 1][0] in valid_down
+        ):
+            maze[x, y][1].append((x, y + 1))
+        if (
+            char in valid_right
+            and maze.get((x - 1, y))
+            and maze[x - 1, y][0] in valid_left
+        ):
+            maze[x, y][1].append((x - 1, y))
+        if (
+            char in valid_left
+            and maze.get((x + 1, y))
+            and maze[x + 1, y][0] in valid_right
+        ):
+            maze[x, y][1].append((x + 1, y))
+        if char == "S":
+            start = space
+
+    search_queue = deque()
+    breadth = 1
+    for neighbor in maze[start][1]:
+        search_queue.append((neighbor, breadth))
+    loop_pipes = [start]
+    while search_queue:
+        pipe, breadth_new = search_queue.popleft()
+        if pipe not in loop_pipes:
+            breadth = breadth_new
+            loop_pipes.append(pipe)
+            for neighbor in maze[pipe][1]:
+                search_queue.append((neighbor, breadth + 1))
+    return loop_pipes, breadth
 
 
 class Solution(StrSplitSolution):
@@ -13,66 +69,19 @@ class Solution(StrSplitSolution):
 
     @answer(6613)
     def part_1(self) -> int:
-        valid_up = "|7FS"
-        valid_down = "|LJS"
-        valid_left = "-LFS"
-        valid_right = "-J7S"
-        maze_size = [0, 0]
-        maze = {}
-        for y, line in enumerate(self.input):
-            for x, char in enumerate(line):
-                if char != ".":
-                    maze[(x, y)] = char, []
-                if x > maze_size[0] or y > maze_size[1]:
-                    maze_size = [x, y]
-        start = ()
-        for space, (char, connected_neighbors) in maze.items():
-            x, y = space
-            if (
-                char in valid_down
-                and maze.get((x, y - 1))
-                and maze[x, y - 1][0] in valid_up
-            ):
-                maze[x, y][1].append((x, y - 1))
-            if (
-                char in valid_up
-                and maze.get((x, y + 1))
-                and maze[x, y + 1][0] in valid_down
-            ):
-                maze[x, y][1].append((x, y + 1))
-            if (
-                char in valid_right
-                and maze.get((x - 1, y))
-                and maze[x - 1, y][0] in valid_left
-            ):
-                maze[x, y][1].append((x - 1, y))
-            if (
-                char in valid_left
-                and maze.get((x + 1, y))
-                and maze[x + 1, y][0] in valid_right
-            ):
-                maze[x, y][1].append((x + 1, y))
-            if char == "S":
-                start = space
-
-        search_queue = deque()
-        breadth = 1
-        for neighbor in maze[start][1]:
-            search_queue.append((neighbor, breadth))
-        searched = [start]
-        while search_queue:
-            pipe, breadth_new = search_queue.popleft()
-            if pipe not in searched:
-                breadth = breadth_new
-                searched.append(pipe)
-                for neighbor in maze[pipe][1]:
-                    search_queue.append((neighbor, breadth + 1))
+        _, breadth = find_loop(self.input)
         return breadth
 
-    # @answer(1234)
+    @answer(511)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        loop_pipes, _ = find_loop(self.input)
+        enclosed = []
+        pointing_north = "|LJS"
+        for y, line in enumerate(self.input):
+            is_inside = False
+            for x, char in enumerate(line):
+                if char in pointing_north and (x, y) in loop_pipes:
+                    is_inside = not is_inside
+                elif (x, y) not in loop_pipes and is_inside:
+                    enclosed.append((x, y))
+        return len(enclosed)
