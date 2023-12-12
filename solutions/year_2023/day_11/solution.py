9a7cbd51403da07b42a3618118c38302d5c2defc
Dan Yeaw <dan@yeaw.me>
2023-12-11 22:16:12 -0500

    Add 2023 day 11 part 1

--- solutions/year_2023/day_11/solution.py
+++ solutions/year_2023/day_11/solution.py
@@ -1,15 +1,49 @@
 # puzzle prompt: https://adventofcode.com/2023/day/11
+from ...base import StrSplitSolution, answer
 
-from ...base import StrSplitSolution
+
+def find_total(galaxies):
+    total = 0
+    for idx, galaxy in enumerate(galaxies):
+        for next_point in galaxies[(idx + 1) :]:
+            distance = abs(next_point[1] - galaxy[1]) + abs(next_point[0] - galaxy[0])
+            total += distance
+    return total
 
 
 class Solution(StrSplitSolution):
     _year = 2023
     _day = 11
 
-    # @answer(1234)
+    @answer(9418609)
     def part_1(self) -> int:
-        return 0
+        image = [[char for char in line] for line in self.input]
+        galaxies: list[list[int]] = []
+        y_expansion = []
+        x_expansion = []
+        for row_num, row in enumerate(image):
+            if "#" not in row:
+                y_expansion.append(row_num)
+        for col_num, col in enumerate(list(rows) for rows in zip(*image)):
+            if "#" not in col:
+                x_expansion.append(col_num)
+        for y, line in enumerate(self.input):
+            for x, char in enumerate(line):
+                if char == "#":
+                    galaxies.append([x, y])
+        expansion = 0
+        for y in y_expansion:
+            for galaxy in galaxies:
+                if galaxy[1] > (y + expansion):
+                    galaxy[1] += 1
+            expansion += 1
+        expansion = 0
+        for x in x_expansion:
+            for galaxy in galaxies:
+                if galaxy[0] > (x + expansion):
+                    galaxy[0] += 1
+            expansion += 1
+        return find_total(galaxies)
 
     # @answer(1234)
     def part_2(self) -> int:
