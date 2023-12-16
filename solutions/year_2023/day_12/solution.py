f1d42b61cba0d5af38713e3c40130b796430df29
Dan Yeaw <dan@yeaw.me>
2023-12-16 17:14:50 -0500

    Add 2023 day 12 part 2

--- solutions/year_2023/day_12/solution.py
+++ solutions/year_2023/day_12/solution.py
@@ -1,14 +1,45 @@
 # puzzle prompt: https://adventofcode.com/2023/day/12
+import functools
 import itertools
 import re
 
 from ...base import StrSplitSolution, answer
 
 
-def valid_arrangement(record: str, group_sizes: list[int]) -> bool:
+@functools.cache
+def valid_arrangement(record: str, pattern: str) -> bool:
+    group_sizes = [int(value) for value in pattern.split(",")]
     actual_groups = re.findall(r"#+", record)
     actual_group_sizes = [len(group) for group in actual_groups]
     return actual_group_sizes == group_sizes
+
+
+@functools.cache
+def number_of_arrangements(record: str, groups: tuple[int]) -> int:
+    if not groups:
+        no_broken_strings_remaining = "#" not in record
+        return 1 if no_broken_strings_remaining else 0
+    group_min_start = 0
+    group_max_start = len(record) - sum(groups) - len(groups) + 1
+    if "#" in record:
+        group_max_start = min(group_max_start, record.index("#"))
+    remaining_groups: list[int]
+    first_group_size, *remaining_groups = groups
+    num_arrangements = 0
+    for group_start in range(group_min_start, group_max_start + 1):
+        group_end = group_start + first_group_size
+        group = record[group_start:group_end]
+
+        is_group_possible = all(char in "#?" for char in group)
+        is_end_of_record = group_end >= len(record)
+        is_group_separated = is_end_of_record or record[group_end] in ".?"
+        if not is_group_possible or not is_group_separated:
+            continue
+        remaining_record_after_sep = record[group_end + 1 :]
+        num_arrangements += number_of_arrangements(
+            remaining_record_after_sep, tuple(remaining_groups)
+        )
+    return num_arrangements
 
 
 class Solution(StrSplitSolution):
@@ -36,10 +67,20 @@ class Solution(StrSplitSolution):
                 record_characters = list(record)
                 for position in assignment:
                     record_characters[position] = "#"
-                if valid_arrangement("".join(record_characters), group_sizes):
+                if valid_arrangement("".join(record_characters), pattern_str):
                     total_arrangements += 1
         return total_arrangements
 
-    # @answer(1234)
+    @answer(2128386729962)
     def part_2(self) -> int:
-        return 0
+        total_arrangements = 0
+        for line in self.input:
+            record, pattern_str = line.split()
+            record = f"{record}?" * 4 + record
+            pattern_str = f"{pattern_str}," * 4 + pattern_str
+            group_sizes = [int(value) for value in pattern_str.split(",")]
+            record = re.sub(r"\.+", ".", record)
+            record = re.sub(r"^\.|\.$", r"", record)
+            total_arrangements += number_of_arrangements(record, tuple(group_sizes))
+
+        return total_arrangements
