59a77e22c85da850a2e761f27c7c85dc32babd33
Dan Yeaw <dan@yeaw.me>
2023-12-17 14:16:41 -0500

    Add 2023 day 13 part 2

--- solutions/year_2023/day_13/README.md
+++ solutions/year_2023/day_13/README.md
@@ -4,10 +4,13 @@
 
 ## Part 1
 
-I was originally finding the reflection point and then comparing each row out from that point. This
-required a lot of checks for finding the max and min row. Zip does this automatically because it stops
-with the shortest iterable. So you can zip the rows in both directions and check that they are all
-equal.
+I was originally finding the reflection point and then comparing each row out
+from that point. This required a lot of checks for finding the max and min row.
+Zip does this automatically because it stops with the shortest iterable. So you
+can zip the rows in both directions and check that they are all equal.
 
 ## Part 2
 
+Instead of checking all the lines are the same on the reflection, we now look
+for smudge. We find the distance between the two would be 1. To find the
+distance we zip up the two rows and sum the differences.
--- solutions/year_2023/day_13/solution.py
+++ solutions/year_2023/day_13/solution.py
@@ -3,18 +3,26 @@
 from ...base import TextSolution, answer
 
 
-def score_block(block: list[list[str]]) -> int:
-    if row := reflection_row(block):
+def distance(above: list[str], below: list[str]) -> int:
+    return sum(a != b for a, b in zip(above, below))
+
+
+def score_block(block: list[list[str]], match_distance: int) -> int:
+    if row := reflection_row(block, match_distance):
         return row * 100
-    return col if (col := reflection_row(list(zip(*block)))) else 0  # type: ignore
+    return col if (col := reflection_row(list(zip(*block)), match_distance)) else 0  # type: ignore
 
 
-def reflection_row(block: list[list[str]]) -> int:
+def reflection_row(block: list[list[str]], match_distance: int) -> int:
     for idx in range(len(block)):
         if idx == 0:
             continue
-        if all(
-            above == below for above, below in zip(reversed(block[:idx]), block[idx:])
+        if (
+            sum(
+                distance(above, below)
+                for above, below in zip(reversed(block[:idx]), block[idx:])
+            )
+            == match_distance
         ):
             return idx
     return 0
@@ -30,12 +38,12 @@ class Solution(TextSolution):
             [list(row) for row in block.split("\n")]
             for block in list(self.input.split("\n\n"))
         ]
-        return sum(score_block(block) for block in blocks)
+        return sum(score_block(block, 0) for block in blocks)
 
     # @answer(1234)
     def part_2(self) -> int:
-        return 0
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        blocks = [
+            [list(row) for row in block.split("\n")]
+            for block in list(self.input.split("\n\n"))
+        ]
+        return sum(score_block(block, 1) for block in blocks)
