d20df4fb61f83d8565ec667ae926480bd066c7d5
Dan Yeaw <dan@yeaw.me>
2023-12-18 20:43:59 -0500

    Finish part 1

--- solutions/year_2023/day_14/solution.py
+++ solutions/year_2023/day_14/solution.py
@@ -1,32 +1,30 @@
 # puzzle prompt: https://adventofcode.com/2023/day/14
 
-from ...base import StrSplitSolution
+from ...base import StrSplitSolution, answer
 
 
 class Solution(StrSplitSolution):
     _year = 2023
     _day = 14
 
-    # @answer(1234)
+    @answer(106648)
     def part_1(self) -> int:
-        map = [list(line) for line in self.input]
-        for line in map:
-            print(line)
-        print()
-        map_by_cols = list(zip(*map))
-        north_tilted = []
-        for col in map_by_cols:
-            if "#" not in col:
-                rounded = col.count("O")
-                empty = len(col) - rounded
-                north_tilted.append(
-                    [c for c in rounded * "O"] + [c for c in empty * "."]
-                )
-            else:
-                pass
-            print(col)
-        print(north_tilted)
-        return 0
+        rock_map = [list(row) for row in self.input]
+        changed = True
+        while changed:
+            changed = False
+            for row_num, row in enumerate(rock_map):
+                if row_num == 0:
+                    continue
+                for col_num, char in enumerate(row):
+                    if char == "O" and rock_map[row_num - 1][col_num] == ".":
+                        rock_map[row_num - 1][col_num] = "O"
+                        rock_map[row_num][col_num] = "."
+                        changed = True
+        return sum(
+            distance * row.count("O")
+            for distance, row in enumerate(reversed(list(rock_map)), start=1)
+        )
 
     # @answer(1234)
     def part_2(self) -> int:
