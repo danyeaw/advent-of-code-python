a0883f0b4fcf6e40bf0e3136e42728797440572c
Dan Yeaw <dan@yeaw.me>
2023-12-21 22:36:49 -0500

    Use StrSplitSolution with separator

--- solutions/year_2023/day_15/solution.py
+++ solutions/year_2023/day_15/solution.py
@@ -1,6 +1,7 @@
 # puzzle prompt: https://adventofcode.com/2023/day/15
+from __future__ import annotations
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
 
 
 def get_hash(step: str) -> int:
@@ -12,14 +13,14 @@ def get_hash(step: str) -> int:
     return step_value
 
 
-class Solution(TextSolution):
+class Solution(StrSplitSolution):
+    separator = ","
     _year = 2023
     _day = 15
 
     @answer(511257)
     def part_1(self) -> int:
-        steps = self.input.split(",")
-        return sum(get_hash(step) for step in steps)
+        return sum(get_hash(step) for step in self.input)
 
     # @answer(1234)
     def part_2(self) -> int:
