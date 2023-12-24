c3629d327454ea3a5c80c2034c047f6463dd9266
Dan Yeaw <dan@yeaw.me>
2023-12-24 10:18:47 -0500

    Update to include State in cost accumulation

--- solutions/year_2023/day_17/solution.py
+++ solutions/year_2023/day_17/solution.py
@@ -3,7 +3,7 @@ import queue
 from dataclasses import dataclass
 from enum import IntEnum
 
-from ...base import StrSplitSolution
+from ...base import StrSplitSolution, answer
 
 GridLocation = tuple[int, int]
 
@@ -75,7 +75,8 @@ def reconstruct_path(
 def heuristic(a: GridLocation, b: GridLocation) -> int:
     x1, y1 = a
     x2, y2 = b
-    return abs(x1 - x2) + abs(y1 - y2)
+    # return abs(x1 - x2) + abs(y1 - y2)
+    return 0
 
 
 @dataclass(frozen=True, order=True)
@@ -138,30 +139,33 @@ class WeightedGrid:
 
 def a_star_search(grid: WeightedGrid, start: GridLocation, goal: GridLocation):
     frontier: queue.PriorityQueue[tuple[int, State]] = queue.PriorityQueue()
-    frontier.put((0, State(start, Direction.NONE, 0)))
-    came_from: dict[GridLocation, GridLocation] = {start: start}
-    cost_so_far: dict[GridLocation, int] = {start: 0}
+    start_state = State(start, Direction.NONE, 0)
+    frontier.put((0, start_state))
+    came_from: dict[State, State] = {start_state: start_state}
+    cost_so_far: dict[State, int] = {State(start, Direction.NONE, 0): 0}
     while not frontier.empty():
         priority, current = frontier.get()
         if current == goal:
             break
         for next_neighbor in grid.neighbors(current):
-            new_cost = cost_so_far[current.position] + grid.cost(
+            new_cost = cost_so_far[current] + grid.cost(
                 current.position, next_neighbor.position
             )
             if (
-                next_neighbor.position not in cost_so_far
-                or new_cost < cost_so_far[next_neighbor.position]
+                next_neighbor not in cost_so_far
+                or new_cost < cost_so_far[next_neighbor]
             ):
-                cost_so_far[next_neighbor.position] = new_cost
+                cost_so_far[next_neighbor] = new_cost
                 priority = new_cost + heuristic(next_neighbor.position, goal)
                 frontier.put((priority, next_neighbor))
-                came_from[next_neighbor.position] = current.position
-    draw_grid(grid, point_to=came_from, start=start, goal=goal)
-    print()
-    draw_grid(grid, path=reconstruct_path(came_from, start=start, goal=goal))
-    print()
-    draw_grid(grid, number=cost_so_far, start=start, goal=goal)
+                came_from[next_neighbor] = current
+    # came_from_location = {k.position: v.position for k, v in came_from.items()}
+    # draw_grid(grid, point_to=came_from_location, start=start, goal=goal)
+    # print(came_from_location)
+    # print()
+    # draw_grid(grid, path=reconstruct_path(came_from_location, start=start, goal=goal))
+    # print()
+    # draw_grid(grid, number=cost_so_far, start=start, goal=goal)
     return came_from, cost_so_far
 
 
@@ -169,7 +173,7 @@ class Solution(StrSplitSolution):
     _year = 2023
     _day = 17
 
-    # @answer(1234)
+    @answer(859)
     def part_1(self) -> int:
         width = len(self.input[0])
         height = len(self.input)
@@ -177,7 +181,9 @@ class Solution(StrSplitSolution):
         weighted_grid = WeightedGrid(width, height, weights)
         start, goal = (0, 0), (width - 1, height - 1)
         came_from, cost_so_far = a_star_search(weighted_grid, start, goal)
-        return max(cost_so_far.values())
+        return min(
+            cost for state, cost in cost_so_far.items() if state.position == goal
+        )
 
     # @answer(1234)
     def part_2(self) -> int:
