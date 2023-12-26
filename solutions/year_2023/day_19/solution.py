83575795798dd0c14fcbdbccb685bdf60aaca6f8
Dan Yeaw <dan@yeaw.me>
2023-12-26 18:37:44 -0500

    Change to using different range solution

--- solutions/year_2023/day_19/solution.py
+++ solutions/year_2023/day_19/solution.py
@@ -1,5 +1,5 @@
 # puzzle prompt: https://adventofcode.com/2023/day/19
-from functools import reduce
+from typing import NamedTuple
 
 from ...base import TextSolution, answer
 
@@ -26,54 +26,72 @@ def is_approved(ratings, workflows: dict[str, str], next_workflow: str) -> bool:
     return False
 
 
-def add_ranges(ranges: dict[str, range], cond: str) -> dict[str, range] | None:
-    category, op, value = cond[0], cond[1], int(cond[2:])
-    rng = ranges.get(category, range(1, 4001))
-    if op == "<" and value <= rng.start or op == ">" and value > (rng.stop - 1):
-        return None
-    elif op == "<":
-        return {category: range(rng.start, value)}
-    return {category: range(value + 1, rng.stop)}
+def apply_less(r: range, value: int):
+    if r.start > value:
+        return range(0), r
+    if r.stop > value:
+        return range(r.start, value), range(value, r.stop)
+    return r, range(0)
 
 
-def inverse(cond: str) -> str:
-    category, op, value = cond[0], cond[1], int(cond[2:])
-    return f"{category}>{value - 1}" if op == "<" else f"{category}<{value + 1}"
+def apply_more(r: range, value: int):
+    if r.stop < value:
+        return range(0), r
+    if r.start <= value:
+        return range(value + 1, r.stop), range(r.start, value + 1)
+    return r, range(0)
 
 
-def trace_path(workflows: dict[str, str], state: tuple[str, dict[str, range]]):
-    workflow_name, ranges = state
-    print(state)
-    workflow = workflows[workflow_name]
-    for step in workflow.split(","):
-        if step == "A":
-            yield ranges
-        elif ":" not in step and step != "R":
-            yield from trace_path(workflows, (step, ranges))
-        elif ":" in step:
-            cond, next_workflow = step.split(":")
-            if next_workflow != "R":
-                if first_ranges := add_ranges(ranges, inverse(cond)):
-                    if step.split(":")[1] == "A":
-                        yield first_ranges
-                    else:
-                        yield from trace_path(
-                            workflows, (step.split(":")[1], first_ranges)
-                        )
-                if second_ranges := add_ranges(ranges, cond):
-                    if step.split(":")[1] == "A":
-                        yield second_ranges
-                    else:
-                        yield from trace_path(
-                            workflows, (step.split(":")[1], second_ranges)
-                        )
+class RangePart(NamedTuple):
+    x: range
+    m: range
+    a: range
+    s: range
+
+    @property
+    def size(self):
+        return len(self.x) * len(self.m) * len(self.a) * len(self.s)
+
+    def _apply(self, func, name: str, value: int):
+        a, b = self._asdict(), self._asdict()
+        a[name], b[name] = func(a[name], value)
+        return RangePart(**a), RangePart(**b)
+
+    def apply_less(self, name: str, value: int):
+        return self._apply(apply_less, name, value)
+
+    def apply_more(self, name: str, value: int):
+        return self._apply(apply_more, name, value)
 
 
-def count_paths(paths):
-    print(paths)
-    return sum(
-        reduce(int.__mul__, [len(rng) + 1 for rng in path.values()]) for path in paths
-    )
+def make_solver2(rules):
+    def run(part: RangePart, name):
+        if part.size == 0:
+            return 0
+        if name == "A":
+            return part.size
+        if name == "R":
+            return 0
+        result = 0
+        print(name)
+        for rule in rules[name]:
+            if ":" in rule:
+                cond, target = rule.split(":")
+                if ">" in cond:
+                    name, value = cond.split(">")
+                    a, part = part.apply_more(name, int(value))
+                    result += run(a, target)
+                elif "<" in cond:
+                    name, value = cond.split("<")
+                    a, part = part.apply_less(name, int(value))
+                    result += run(a, target)
+                else:
+                    assert 0, rule
+            else:
+                result += run(part, rule)
+        return result
+
+    return run
 
 
 class Solution(TextSolution):
@@ -94,12 +112,18 @@ class Solution(TextSolution):
         parts = (tuple(int(value[2:]) for value in part) for part in parts)
         return sum(sum(part) for part in parts if is_approved(part, workflows, "in"))
 
-    # @answer(1234)
+    @answer(121964982771486)
     def part_2(self) -> int:
         blocks = self.input.split("\n\n")
         workflows = {
-            line.split("{")[0]: line.split("{")[1][:-1]
+            line.split("{")[0]: line.split("{")[1][:-1].split(",")
             for line in blocks[0].split("\n")
         }
-        state = ("in", {category: range(1, 4001) for category in "xmas"})
-        return count_paths(trace_path(workflows, state))
+        part = RangePart(
+            x=range(1, 4001),
+            m=range(1, 4001),
+            a=range(1, 4001),
+            s=range(1, 4001),
+        )
+        run = make_solver2(workflows)
+        return run(part, "in")
