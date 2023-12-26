b3f9273e0530892b647418a5057ff67dfb0f5de4
Dan Yeaw <dan@yeaw.me>
2023-12-25 19:41:02 -0500

    Refactor and simplify solution

--- solutions/year_2023/day_19/solution.py
+++ solutions/year_2023/day_19/solution.py
@@ -1,8 +1,27 @@
 # puzzle prompt: https://adventofcode.com/2023/day/19
-import collections
-from collections import namedtuple
-
 from ...base import TextSolution, answer
+
+
+def is_approved(ratings, workflows: dict[str, str], next_workflow: str) -> bool:
+    workflow = workflows[next_workflow]
+    x, m, a, s = ratings
+    for step in workflow.split(","):
+        if step == "R":
+            return False
+        if step == "A":
+            return True
+        if ":" not in step:
+            return is_approved(ratings, workflows, step)
+        cond = step.split(":")[0]
+        if eval(cond):
+            if step.split(":")[1] == "R":
+                return False
+            return (
+                True
+                if step.split(":")[1] == "A"
+                else is_approved(ratings, workflows, step.split(":")[1])
+            )
+    return False
 
 
 class Solution(TextSolution):
@@ -12,60 +31,16 @@ class Solution(TextSolution):
     @answer(399284)
     def part_1(self) -> int:
         blocks = self.input.split("\n\n")
-        workflows_list = [
-            workflow.replace("}", "").split("{") for workflow in blocks[0].split("\n")
-        ]
-        workflows = {workflow[0]: workflow[1].split(",") for workflow in workflows_list}
-        for workflow_name, workflow in workflows.items():
-            for idx, step in enumerate(workflow):
-                if ":" in step:
-                    split_step = step.split(":")
-                    workflows[workflow_name][idx] = split_step
-
-        Rating = namedtuple("Rating", ["x", "m", "a", "s"])
-        ratings = [
+        workflows = {
+            line.split("{")[0]: line.split("{")[1][:-1]
+            for line in blocks[0].split("\n")
+        }
+        parts = (
             rating.replace("{", "").replace("}", "").split(",")
             for rating in blocks[1:][0].split("\n")
-        ]
-        ratings = [Rating(*[int(value[2:]) for value in rating]) for rating in ratings]
-        accepted = []
-        for rating in ratings:
-            queue = collections.deque(["in"])
-            while queue:
-                workflow = queue.pop()
-                for step in workflows[workflow]:
-                    if step == "A":
-                        accepted.append(rating)
-                        queue.clear()
-                        break
-                    elif step == "R":
-                        queue.clear()
-                        break
-                    elif ">" in step[0]:
-                        field, value = step[0].split(">")
-                        if getattr(rating, field) > int(value):
-                            if step[1] == "A":
-                                accepted.append(rating)
-                                queue.clear()
-                            elif step[1] == "R":
-                                queue.clear()
-                            else:
-                                queue.append(step[1])
-                            break
-                    elif "<" in step[0]:
-                        field, value = step[0].split("<")
-                        if getattr(rating, field) < int(value):
-                            if step[1] == "A":
-                                accepted.append(rating)
-                                queue.clear()
-                            elif step[1] == "R":
-                                queue.clear()
-                            else:
-                                queue.append(step[1])
-                            break
-                    else:
-                        queue.append(step)
-        return sum(sum(rating) for rating in accepted)
+        )
+        parts = (tuple(int(value[2:]) for value in part) for part in parts)
+        return sum(sum(part) for part in parts if is_approved(part, workflows, "in"))
 
     # @answer(1234)
     def part_2(self) -> int:
