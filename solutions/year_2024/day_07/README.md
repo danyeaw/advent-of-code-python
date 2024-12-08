878beb82c9a9ae25e458ca5b83f7dcc80355a882
Dan Yeaw <dan@yeaw.me>
2024-12-08 09:14:50 -0500

    Add 2024 day 7 solution

---
+++ solutions/year_2024/day_07/README.md
@@ -0,0 +1,8 @@
+# Day 7 (2024)
+
+`BRIDGE REPAIR` ([prompt](https://adventofcode.com/2024/day/7))
+
+## Part 1
+
+## Part 2
+
---
+++ solutions/year_2024/day_07/solution.py
@@ -0,0 +1,49 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+# puzzle prompt: https://adventofcode.com/2024/day/7
+from collections.abc import Sequence
+from itertools import product
+from operator import add, mul
+from typing import Callable
+
+from ...base import StrSplitSolution, answer
+from ...utils.parsing import parse_int_list
+
+
+def concat(a: int, b: int) -> int:
+    return int(str(a) + str(b))
+
+
+def process_ops(nums: list[int], ops: Sequence[Callable[[int, int], int]]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+    left, right, *rest = nums
+    cur_op, *remaining_ops = ops
+    return process_ops([cur_op(left, right), *rest], remaining_ops)
+
+
+def process_line(line: str, enable_concat=False) -> int:
+    target, *inputs = parse_int_list(line.replace(":", "").split())
+    ops = [add, mul]
+    if enable_concat:
+        ops.append(concat)
+    if any(
+        process_ops(inputs, op_combo) == target
+        for op_combo in product(ops, repeat=len(inputs) - 1)
+    ):
+        return target
+    return 0
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 7
+
+    @answer(882304362421)
+    def part_1(self) -> int:
+        return sum(process_line(calibration) for calibration in self.input)
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        return sum(
+            process_line(calibration, enable_concat=True) for calibration in self.input
+        )
