9c1e873c28d1b069eeaf0f571634241970ca2f60
Dan Yeaw <dan@yeaw.me>
2024-12-07 19:02:15 -0500

    Add 2024 day 5 part 2

--- solutions/year_2024/day_05/README.md
+++ solutions/year_2024/day_05/README.md
@@ -1,10 +1,22 @@
 # Day 5 (2024)
 
-`TITLE` ([prompt](https://adventofcode.com/2024/day/5))
+`PRINT QUEUE` ([prompt](https://adventofcode.com/2024/day/5))
 
-Use this space for notes on the day's solution and to document what you've learned!
+The graphlib TopologicalGraph really helped for this one in Python.
 
 ## Part 1
 
+I was originally trying to make this one way too complicated. I originally was trying to create a dictionary
+graph for the rules where the X part of the rule was the key and the Y parts were values added to a list.
+I was then trying to create a sorted list of all numbers found in all the updates using this graph. It worked
+for the test input, but I wasn't getting the right answer for the full input, probably due to issues with my
+sorting algorithm.
+
+Instead, we can run each update through each rule to see if it passes all of them.
+
 ## Part 2
 
+I learned about graphlib TopologicalGraph for this one. This is similar to what I was trying to do in part1
+initially, but it manages sorting for you. We also don't need to sort all numbers found in all updates, we
+only need to sort each update that doesn't pass the rules using only the rules that have both their numbers
+in the update.
--- solutions/year_2024/day_05/solution.py
+++ solutions/year_2024/day_05/solution.py
@@ -1,7 +1,7 @@
 # Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
-# from graphlib import TopologicalSorter
-
 # puzzle prompt: https://adventofcode.com/2024/day/5
+from graphlib import TopologicalSorter
+
 from ...base import TextSolution, answer
 from ...utils.parsing import parse_int_list
 
@@ -13,28 +13,32 @@ def adheres_to_rules(rule: list[int], update: list[int]) -> bool:
     return True
 
 
+def middle_element(array: list[int]) -> int:
+    return array[len(array) // 2]
+
+
 class Solution(TextSolution):
     _year = 2024
     _day = 5
 
-    @answer(7024)
-    def part_1(self) -> int:
+    @answer((7024, 4151))
+    def solve(self) -> tuple[int, int]:
         input_rules, input_updates = self.input.split("\n\n")
         rules = [parse_int_list(rule.split("|")) for rule in input_rules.splitlines()]
-        total = 0
+        part1, part2 = 0, 0
         updates = [
             parse_int_list(raw_update.split(","))
             for raw_update in input_updates.splitlines()
         ]
         for update in updates:
             if all(adheres_to_rules(rule, update) for rule in rules):
-                total += update[(len(update) - 1) // 2]
-        return total
+                part1 += middle_element(update)
+            else:
+                topo_sorter: TopologicalSorter = TopologicalSorter()
+                for left, right in rules:
+                    if left in update and right in update:
+                        topo_sorter.add(left, right)
+                fixed_update = list(topo_sorter.static_order())
+                part2 += middle_element(fixed_update)
 
-    # @answer(1234)
-    def part_2(self) -> int:
-        return 0
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return part1, part2
