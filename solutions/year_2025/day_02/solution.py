3cf89ecf355872dd2649b67ac003ec33f7298b64
Dan Yeaw <dan@yeaw.me>
2025-12-04 19:37:29 -0500

    Simply solution

--- solutions/year_2025/day_02/solution.py
+++ solutions/year_2025/day_02/solution.py
@@ -6,22 +6,20 @@ from ...base import StrSplitSolution, answer
 
 
 def find_invalid(start: int, stop: int) -> int:
-    invalid = []
-    for num in range(start, stop + 1):
-        num_str = str(num)
-        mid_point = len(num_str) // 2
-        if num_str[0] == "0" or num_str[:mid_point] == num_str[mid_point:]:
-            invalid.append(num)
-    return sum(invalid)
+    return sum(
+        num
+        for num in range(start, stop + 1)
+        if (num_str := str(num))[0] == "0"
+        or num_str[: len(num_str) // 2] == num_str[len(num_str) // 2 :]
+    )
 
 
 def find_invalid_twice(start: int, stop: int) -> int:
-    invalid = []
-    for num in range(start, stop + 1):
-        num_str = str(num)
-        if num_str[0] == "0" or num_str in (num_str + num_str)[1:-1]:
-            invalid.append(num)
-    return sum(invalid)
+    return sum(
+        num
+        for num in range(start, stop + 1)
+        if (num_str := str(num))[0] == "0" or num_str in (num_str + num_str)[1:-1]
+    )
 
 
 class Solution(StrSplitSolution):
@@ -29,20 +27,18 @@ class Solution(StrSplitSolution):
     _day = 2
     separator = ","
 
+    def parse_ranges(self):
+        return [
+            tuple[int, ...](int(value) for value in values.split("-"))
+            for values in self.input
+        ]
+
     @answer(40055209690)
     def part_1(self) -> int:
-        ranges = [
-            tuple(int(value) for value in values.split("-")) for values in self.input
-        ]
-        return sum(find_invalid(range_[0], range_[1]) for range_ in ranges)
+        return sum(find_invalid(start, stop) for start, stop in self.parse_ranges())
 
     @answer(50857215650)
     def part_2(self) -> int:
-        ranges = [
-            tuple(int(value) for value in values.split("-")) for values in self.input
-        ]
-        return sum(find_invalid_twice(range_[0], range_[1]) for range_ in ranges)
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return sum(
+            find_invalid_twice(start, stop) for start, stop in self.parse_ranges()
+        )
