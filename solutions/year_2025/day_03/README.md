35d700c264930cbb9143835e788bcb9904fac537
Dan Yeaw <dan@yeaw.me>
2025-12-04 21:56:42 -0500

    Add 2025 day 3 part 2

--- solutions/year_2025/day_03/README.md
+++ solutions/year_2025/day_03/README.md
@@ -6,5 +6,16 @@ Use this space for notes on the day's solution and to document what you've learn
 
 ## Part 1
 
+Since we only need 2 digits, we can use nested for loops with taking the
+second loop one to the right of the first and iterating through the whole
+battery bank.
+
 ## Part 2
+
+I first tried to do the same method, but this requires combinational
+possibilities to brute force. Instead, a greedy algorithm can be used
+to find the max value from the start until the end with room for the
+number of digits needed. Then find the first index with that max value,
+and then keep going through all the digits by putting the start for the
+next one after the index from the previous one.
 
--- solutions/year_2025/day_03/solution.py
+++ solutions/year_2025/day_03/solution.py
@@ -5,27 +5,29 @@
 from ...base import StrSplitSolution, answer
 
 
-def find_highest_two(numbers: list[int]) -> int:
-    return max(
-        int(f"{numbers[i]}{numbers[j]}")
-        for i in range(len(numbers))
-        for j in range(i + 1, len(numbers))
-    )
+def find_highest(numbers: list[int], size: int) -> int:
+    start = 0
+    result = []
+    for i in range(size):
+        end = len(numbers) - (size - i - 1)
+        max_val = max(numbers[start:end])
+        max_idx = numbers.index(max_val, start)
+        result.append(max_val)
+        start = max_idx + 1
+    return int("".join(map[str](str, result)))
 
 
 class Solution(StrSplitSolution):
     _year = 2025
     _day = 3
 
+    def parse_batteries(self):
+        return [[int(cell) for cell in bank] for bank in self.input]
+
     @answer(17445)
     def part_1(self) -> int:
-        batteries = [[int(cell) for cell in bank] for bank in self.input]
-        return sum(find_highest_two(bank) for bank in batteries)
+        return sum(find_highest(bank, 2) for bank in self.parse_batteries())
 
-    # @answer(1234)
+    @answer(173229689350551)
     def part_2(self) -> int:
-        return 0
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return sum(find_highest(bank, 12) for bank in self.parse_batteries())
