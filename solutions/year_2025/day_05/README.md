7fce59df544eb65248edd7f53a8abcbd5d75cdac
Dan Yeaw <dan@yeaw.me>
2025-12-06 09:22:31 -0500

    Initial solution day 5 part 1

---
+++ solutions/year_2025/day_05/README.md
@@ -0,0 +1,14 @@
+# Day 5 (2025)
+
+`CAFETERIA` ([prompt](https://adventofcode.com/2025/day/5))
+
+Use this space for notes on the day's solution and to document what you've learned!
+
+## Part 1
+
+I was originally trying to expand the ranges in to a list of all the possible numbers. This
+worked with the test data, but it took way too long with the full data set. You can directly
+check if a number is in a list of ranges without expanding all the ranges.
+
+## Part 2
+
---
+++ solutions/year_2025/day_05/solution.py
@@ -0,0 +1,31 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2025/day/5
+
+from ...base import TextSolution, answer
+
+
+class Solution(TextSolution):
+    _year = 2025
+    _day = 5
+
+    @answer(607)
+    def part_1(self) -> int:
+        fresh_ranges_str, available_ids_str = self.input.split("\n\n")
+        fresh_tuples = [
+            tuple(int(num) for num in nums_str.split("-"))
+            for nums_str in fresh_ranges_str.split("\n")
+        ]
+        fresh_ranges = [
+            range(id_range[0], id_range[1] + 1) for id_range in fresh_tuples
+        ]
+        available_ids = [int(available) for available in available_ids_str.split("\n")]
+        return sum(1 for i in available_ids if any(i in rng for rng in fresh_ranges))
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        return 0
+
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
