274e0bd418fabbe2c284303d66554dafaaf8b10b
Dan Yeaw <dan@yeaw.me>
2025-12-06 09:25:29 -0500

    Simplify with StrSplitSolution

--- solutions/year_2025/day_05/solution.py
+++ solutions/year_2025/day_05/solution.py
@@ -2,16 +2,17 @@
 
 # puzzle prompt: https://adventofcode.com/2025/day/5
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
 
 
-class Solution(TextSolution):
+class Solution(StrSplitSolution):
     _year = 2025
     _day = 5
+    separator = "\n\n"
 
     @answer(607)
     def part_1(self) -> int:
-        fresh_ranges_str, available_ids_str = self.input.split("\n\n")
+        fresh_ranges_str, available_ids_str = self.input
         fresh_tuples = [
             tuple(int(num) for num in nums_str.split("-"))
             for nums_str in fresh_ranges_str.split("\n")
